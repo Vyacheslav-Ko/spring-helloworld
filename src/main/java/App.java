30542c02224cd6878a1cc06ed6b90182f881163f
Vyacheslav-Ko <Ignition1978@gmail.com>
2020-08-03 09:03:32 +0500

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanWorldTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(beanWorldTwo));
+
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getVoice());
+        Cat beanCatTwo =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.equals(beanCatTwo));
+
     }
 }
